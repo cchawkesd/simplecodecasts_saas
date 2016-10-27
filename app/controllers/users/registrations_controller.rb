5b256eeb4fbd67b541ea396c83a20ead5d4ef67b
David Hawkes <nottmhawk@yahoo.co.uk>
2016-10-27 20:37:41 +0000

    Update routes file with new devise controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
         if params[:plan]
           resource.plan_id = params[:plan]
         if resource.plan_id = 2
-          resource save_with_payment
+          resource.save_with_payment
         else
-          resource save
+          resource.save
         end
         end
      end
